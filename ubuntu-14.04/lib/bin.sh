5cc7ba2672c1d3a6c9a800d198ab1a43e3d2855f
xcjs <zlowery@xcjs.com>
2015-05-07 21:49:40 -0400

    Added bin.sh to install custom binaries.

---
+++ ubuntu-14.04/lib/bin.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# Custom binaries ##############################################################
+
+cd ~/
+mkdir -p bin
+cd ~/bin
+
+# JetBrains
+
+INTELLIJDOWNLOAD=ideaIU-14.1.2.tar.gz
+PHPSTORMDOWNLOAD=PhpStorm-8.0.3.tar.gz
+
+INTELLIJINSTALL=intellij-ide
+PHPSTORMINSTALL=phpstorm-ide
+
+wget https://download.jetbrains.com/idea/$INTELLIJDOWNLOAD
+mkdir $INTELLIJINSTALL
+tar -zxvf $INTELLIJDOWNLOAD --strip-components=1 -C intellij-ide
+
+wget https://download.jetbrains.com/webide/$PHPSTORMDOWNLOAD
+mkdir $PHPSTORMINSTALL
+tar -zxvf $PHPSTORMDOWNLOAD --strip-components=1 -C phpstorm-ide
+
+rm $INTELLIJDOWNLOAD $PHPSTORMDOWNLOAD
+
+# Android SDK
+
+ANDROIDSDKDOWNLOAD=android-sdk_r24.1.2-linux.tgz
+
+wget http://dl.google.com/android/$ANDROIDSDKDOWNLOAD
+tar -zxvf $ANDROIDSDKDOWNLOAD
+
+rm $ANDROIDSDKDOWNLOAD
+
+# Setup symlinks to make applications executable from path (Ubuntu ~/.profile
+# adds ~/bin to path if it exists.
+
+ln -s intellij-ide/bin/idea.sh intellij
+ln -s phpstorm-ide/bin/phpstorm.sh phpstorm
+ln -s android-studio/bin/studio.sh androidstudio
