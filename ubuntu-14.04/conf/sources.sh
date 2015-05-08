fe9d96d7e1d29af3a3bd79cb7ccacbfeb30ec719
xcjs <zlowery@xcjs.com>
2015-05-07 21:24:10 -0400

    Added echo statements for non-PPA software channels.

--- ubuntu-14.04/conf/sources.sh
+++ ubuntu-14.04/conf/sources.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Grab the latest version of Virtualbox from the Oracle repository.
+echo "Adding Oracle channel for Virtualbox 4.3..."
 wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list.d/virtualbox.list'
 
 # Grab the offical Google Chrome browser
+echo "Adding Google's official Chrome channel..."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
