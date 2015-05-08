fc0627352b033bc9514e6119c47ec4a691e397b3
xcjs <zlowery@xcjs.com>
2015-05-07 22:02:16 -0400

    Added node.sh for additional node configuration.

---
+++ ubuntu-14.04/lib/node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update npm
+sudo npm install -g npm
+
+# Install commonly used npm packages globally
+sudo npm install -g grunt-cli
+sudo npm install -g bower
+sudo npm install -g sails
