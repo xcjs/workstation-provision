a96182cdd6cc65e2b66abce24539b6ab29596d81
xcjs <zlowery@xcjs.com>
2015-05-15 18:57:06 -0400

    Moved the composer alias to ~/.bash_aliases.

--- ubuntu-14.04/lib/php.sh
+++ ubuntu-14.04/lib/php.sh
@@ -2,5 +2,6 @@
 
 # Install Composer and add it to the path
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin
-echo "alias composer='php /usr/local/bin/composer.phar'" >> ~/.bashrc
-source ~/.bashrc
+touch ~/.bash_aliases
+echo "alias composer='php /usr/local/bin/composer.phar'" >> ~/.bash_aliases
+source ~/.bash_aliases
