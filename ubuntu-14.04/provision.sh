1a7f416455547a0ddfdf61894bf53a4e278d5f29
xcjs <zlowery@xcjs.com>
2015-05-28 16:48:45 -0400

    Removed worse than useless newline function when a standard echo statement would do.

--- ubuntu-14.04/provision.sh
+++ ubuntu-14.04/provision.sh
@@ -16,7 +16,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Load functions
 source ./lib/read_lst.sh
-source ./lib/newline.sh
 
 # Package setup
 
@@ -28,17 +27,17 @@ PPAPACKAGES=$(read_lst "./conf/ppa-packages.lst")
 PACKAGES=$(read_lst "./conf/packages.lst")
 GITREPOS=$(read_lst "./conf/git-repos.lst") 
 
-newline
+echo
 echo "1. Refresh Package Archives ============================================="
 echo "Updating the local package cache..."
 sudo -E apt-get -qq update > /dev/null
 
-newline
+echo
 echo "2. Install OS Updates ==================================================="
 echo "Checking for and installing operating system upates..."
 sudo -E apt-get -qq upgrade > /dev/null && sudo -E apt-get -qq dist-upgrade > /dev/null
 
-newline
+echo
 echo "3. Install Selected Packages ============================================"
 printf %s "$PACKAGES" | while IFS= read -r package
 do
@@ -46,7 +45,7 @@ do
    sudo -E apt-get -qq install $package > /dev/null
 done
 
-newline
+echo
 echo "4. Add Personal Package Archives  ======================================="
 printf %s "$PPAS" | while IFS= read -r ppa
 do
@@ -54,12 +53,12 @@ do
 	sudo -E add-apt-repository -y $ppa
 done
 
-newline
+echo
 echo "5. Refresh Package Cache and Install Custom PPA Updates ================="
 echo "Checking for and installing third party PPA package updates..."
 sudo apt-get -qq update && sudo -E apt-get -qq upgrade > /dev/null && sudo -E apt-get -qq dist-upgrade > /dev/null
 
-newline
+echo
 echo "6. Install Custom PPA Packages =========================================="
 printf %s "$PPAPACKAGES" | while IFS= read -r package
 do
@@ -67,11 +66,11 @@ do
    sudo -E apt-get -qq install $package > /dev/null
 done
 
-newline
+echo
 echo "7. Add and Install Software Sources without a PPA ======================="
 ./conf/sources.sh
 
-newline
+echo
 echo "8. Add and Install Software from Git Repositories ======================="
 printf %s "$GITREPOS" | while IFS= read -r repo
 do
@@ -81,7 +80,7 @@ done
 
 # Create commonly utilized directories
 
-newline
+echo
 echo "9. Create Custom Home Directories ======================================="
 mkdir ~/bin ~/Projects ~/src ~/www ~/.icons ~/.themes
 mkdir -p ~/Pictures/UI
@@ -90,7 +89,7 @@ mkdir -p ~/Pictures/UI
 sudo ln -s ~/.icons /root/.icons
 sudo ln -s ~/.themes /root/.themes 
 
-newline
+echo
 echo "10. Further Configure Installed Software Pacakges ======================="
 SCRIPTPATH=$(dirname `realpath $0`)
 cd $SCRIPTPATH
@@ -99,7 +98,7 @@ cd $SCRIPTPATH
 ./lib/vagrant.sh
 
 # Save the biggest for last...
-newline
+echo
 echo "11. Download and Install Binaries with no Software Channel =============="
 cd $SCRIPTPATH
 ./lib/bin.sh
