b1591769a02bc3812cdd9d7d8fceb14bf427d39c
xcjs <zlowery@xcjs.com>
2015-05-28 20:01:39 -0400

    Corrected misspelled "updates".

--- ubuntu-14.04/provision.sh
+++ ubuntu-14.04/provision.sh
@@ -36,7 +36,7 @@ sudo -E apt-get -qq update 2> /dev/null > /dev/null
 
 echo
 echo "2. Install OS Updates ==================================================="
-echo "Checking for and installing operating system upates..."
+echo "Checking for and installing operating system updates..."
 sudo -E apt-get -qq upgrade 2> /dev/null > /dev/null && sudo -E apt-get -qq dist-upgrade > /dev/null
 
 echo
