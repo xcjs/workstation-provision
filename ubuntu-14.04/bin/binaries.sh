989b77cf7a3ae8516e05047a3f97b665b3f3a7a4
xcjs <zlowery@xcjs.com>
2016-02-05 16:11:09 -0500

    Added code to remove the empty VSCode folder once it's moved up a directory.

--- ubuntu-14.04/bin/binaries.sh
+++ ubuntu-14.04/bin/binaries.sh
@@ -63,7 +63,9 @@ echo "Extracting Visual Studio Code..."
 unzip -qq VSCode-linux64.zip -d vscode-editor
 
 echo "Playing musical chairs with the Visual Studio Code directory..."
+VSCODEDIR=$(ls vscode-editor/ | head -1)
 mv vscode-editor/*/* ./vscode-editor/
+rmdir vscode-editor/${VSCODEDIR}
 
 echo "Symlinking Visual Studio Code..."
 ln -s vscode-editor/vscode vscode
