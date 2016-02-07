44ad3dc5d416d897f9d5f23386fbc3c7ff459650
xcjs <zlowery@xcjs.com>
2016-02-07 18:52:46 -0500

    Reformatted the Intellij script to fit the new format.

--- ubuntu-14.04/bin/intellij.sh
+++ ubuntu-14.04/bin/intellij.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-INTELLIJDOWNLOAD=ideaIU-15.0.3.tar.gz
-INTELLIJINSTALL=intellij-ide
+FILE=ideaIU-15.0.3.tar.gz
+INSTALLPATH=intellij-ide
+
+ORIGINALPATH=$(pwd)
+
+mkdir -p ~/bin
+cd ~/bin
 
 echo "Downloading Intellij..."
-wget -q https://download.jetbrains.com/idea/$INTELLIJDOWNLOAD
+wget -q https://download.jetbrains.com/idea/${FILE}
 
 echo "Extracting Intellij..."
-mkdir $INTELLIJINSTALL
-tar -zxvf $INTELLIJDOWNLOAD --strip-components=1 -C intellij-ide > /dev/null
+mkdir ${INSTALLPATH}
+tar -zxvf ${FILE} --strip-components=1 -C ${INSTALLPATH} > /dev/null
 
 echo "Cleaning up the Intellij archive..."
-rm $INTELLIJDOWNLOAD
+rm ${FILE}
 
 echo "Symlinking Intellij..."
-ln -s ${INTELLIJINSTALL}/bin/idea.sh intellij
+ln -s ${INSTALLPATH}/bin/idea.sh intellij
+
+cd ${ORIGINALPATH}
