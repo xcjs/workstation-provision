8c39bcd20f1b0038b99cb822486a6c195c072acb
Zackary Lowery <zlowery@xcjs.com>
2016-02-07 16:54:40 -0500

    Changes to consolas to properly use the tmp directory.

--- ubuntu-14.04/bin/consolas.sh
+++ ubuntu-14.04/bin/consolas.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
+FILE=PowerPointViewer.exe
+EXTRACTEDFILE=ppviewer.cab
+POWERPOINTVIEWERURL=http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/${FILE}
+
+TMPFILE=$(mktemp /tmp/${FILE}.XXXXXXXXXX)
+
+ORIGINALPATH=$(pwd)
+
 echo "= Install the Consolas Font ============================================="
+
+mkdir -p ~/.fonts
 
 echo "Installing prequiresites to extract the font..."
 sudo apt-get -qq install font-manager cabextract > /dev/null
 
-TEMPPATH='powerpointviewertmp/'
-
-mkdir -p $TEMPPATH
-cd $TEMPPATH
-
 echo "Downloading Microsoft PowerPointViewer..."
-wget -q http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
+wget -q ${POWERPOINTVIEWERURL} -O ${TMPFILE}
+
+cd /tmp
 
 echo "Extracting ppviewer.cab from PowerPointViewer..."
-cabextract -q -L -F ppviewer.cab PowerPointViewer.exe
+cabextract -q -L -F ${EXTRACTEDFILE} ${TMPFILE}
 
 echo "Extracting ppviewer.cab..."
-cabextract -q ppviewer.cab
+cabextract -q ${EXTRACTEDFILE}
 
-mkdir -p ~/.fonts
+rm ${EXTRACTEDFILE}
 
 echo "Copying Consolas to ~/.fonts/"
 cp CONSOLA*.TTF ~/.fonts/
@@ -27,4 +34,4 @@ cp CONSOLA*.TTF ~/.fonts/
 
 echo "Removing the PowerPointViewer..."
 cd ../
-rm -r $TEMPPATH
+rm $TMPFILE
