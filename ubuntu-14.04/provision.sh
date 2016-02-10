e54c53d64d0a3e001a8c870f4ec6a1c541ddee83
xcjs <zlowery@xcjs.com>
2016-02-09 20:32:08 -0500

    Added detection of pre and post-install scripts.

--- ubuntu-14.04/provision.sh
+++ ubuntu-14.04/provision.sh
@@ -68,8 +68,26 @@ echo
 echo "6. Install Custom PPA Packages =========================================="
 printf %s "$PPAPACKAGES" | while IFS= read -r package
 do
-   echo "Installing $package..."
-   sudo -E apt-get -qq install $package 2>&1 /dev/null
+	if test -x "./pre/${package}.sh"
+		then
+			echo 
+			echo "There is a script to run before installing ${package}."
+			echo "Executing ./pre/${package}.sh..."
+			./pre/${package}.sh
+			echo
+	fi
+
+	echo "Installing $package..."
+	sudo -E apt-get -qq install $package 2>&1 /dev/null
+
+	if test -x "./post/${package}.sh"
+		then 
+			echo
+			echo "There is a script to run after installing ${package}."
+			echo "Executing ./post/${package}.sh..."
+			./post/${package}.sh
+			echo
+	fi
 done
 
 echo
@@ -95,14 +113,6 @@ mkdir -p ~/Pictures/UI
 cd ~/
 sudo ln -s .icons /root/.icons
 sudo ln -s .themes /root/.themes 
-
-echo
-echo "10. Further Configure Installed Software Pacakges ======================="
-cd $SCRIPTPATH
-
-./post/node.sh
-./post/php.sh
-./post/vagrant.sh
 
 # Save the biggest for last...
 echo
