3c15161cd5e6e24c54a8a9321924de2762843ee4
xcjs <zlowery@xcjs.com>
2016-02-15 20:45:29 -0500

    Included additional useful Vagrant plugins.

--- ubuntu-14.04/post/vagrant.sh
+++ ubuntu-14.04/post/vagrant.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Install Ruby gem prerequsisites.
+echo "Installing Ruby gem prerequsisites..."
 sudo apt-get -qq install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool /dev/null 2>&1
 
-sudo vagrant plugin install vagrant-linode
+echo "Installing vagrant-vbguest..."
+sudo vagrant plugin install vagrant-vbguest > /dev/null
+
+echo "Installing vagrant-digitalocean..."
+sudo vagrant plugin install vagrant-digitalocean > /dev/null
+
+echo "Installing vagrant-linode..."
+sudo vagrant plugin install vagrant-linode > /dev/null
