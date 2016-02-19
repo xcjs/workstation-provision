8b21fde46c2e68fcd8d8f29a383683db23be06f5
xcjs <zlowery@xcjs.com>
2016-02-19 18:10:14 -0500

    Corrected missing pipe to redirect apt-get output to /dev/null

--- ubuntu-14.04/post/vagrant.sh
+++ ubuntu-14.04/post/vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing Ruby gem prerequsisites..."
-sudo apt-get -qq install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool /dev/null 2>&1
+sudo apt-get -qq install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool > /dev/null 2>&1
 
 echo "Installing vagrant-vbguest..."
 sudo vagrant plugin install vagrant-vbguest > /dev/null
