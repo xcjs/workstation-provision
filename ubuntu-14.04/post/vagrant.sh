c50a099cb271a5b3a4f8d21478c593ddf678b3af
xcjs <zlowery@xcjs.com>
2016-02-21 15:01:15 -0500

    The post-vagrant script now refreshes the command cache to allow builds.

--- ubuntu-14.04/post/vagrant.sh
+++ ubuntu-14.04/post/vagrant.sh
@@ -3,6 +3,9 @@
 echo "Installing Ruby gem prerequsisites..."
 sudo apt-get -qq install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool > /dev/null 2>&1
 
+echo "Refreshing the command cache..."
+hash -r
+
 echo "Installing vagrant-vbguest..."
 sudo vagrant plugin install vagrant-vbguest > /dev/null
 
