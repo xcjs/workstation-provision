931d95b88bf5ec2f8f72a49f12475da749011936
xcjs <zlowery@xcjs.com>
2016-02-27 16:00:22 -0500

    Comment typo correction.

--- dev-mobile/ubuntu-14.04/functions/read_lst.sh
+++ dev-mobile/ubuntu-14.04/functions/read_lst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read_lst () {
-	# grep while ignoring blank lines or lines that begin eith #
+	# grep while ignoring blank lines or lines that begin with #
 	#
 	# read also ends when a newline cannot be found, so append one to the end
 	# to ensure that the last line is parsed.
