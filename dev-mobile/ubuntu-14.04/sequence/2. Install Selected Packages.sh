2466060a55f2799d5bf8d3f6973c35139d0bd96a
xcjs <zlowery@xcjs.com>
2016-02-29 21:21:18 -0500

    White-space formatting.

--- dev-mobile/ubuntu-14.04/sequence/2. Install Selected Packages.sh
+++ dev-mobile/ubuntu-14.04/sequence/2. Install Selected Packages.sh
@@ -3,8 +3,7 @@
 PACKAGES=$(read_lst "./conf/packages.lst")
 
 printf %s "$PACKAGES" | while IFS= read -r package; do
-	if test -x "./pre/${package}.sh"
-	then
+	if test -x "./pre/${package}.sh"; then
 		echo 
 		echo "There is a script to run before installing ${package}."
 		echo "Executing ./pre/${package}.sh..."
@@ -15,12 +14,11 @@ printf %s "$PACKAGES" | while IFS= read -r package; do
 	echo "Installing $package..."
 	sudo -E apt-get -qq install $package > /dev/null 2>&1
 
-	if test -x "./post/${package}.sh"
-		then 
-			echo
-			echo "There is a script to run after installing ${package}."
-			echo "Executing ./post/${package}.sh..."
-			./post/${package}.sh
-			echo
+	if test -x "./post/${package}.sh"; then 
+		echo
+		echo "There is a script to run after installing ${package}."
+		echo "Executing ./post/${package}.sh..."
+		./post/${package}.sh
+		echo
 	fi
 done
