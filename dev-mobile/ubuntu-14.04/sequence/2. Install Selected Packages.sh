08c4d55138985289b9f881ee6dc3917ab79d5955
xcjs <zlowery@xcjs.com>
2016-03-02 18:53:23 -0500

    Escaped pre/post script paths in the unlikely case of invalid characters.

--- dev-mobile/ubuntu-14.04/sequence/2. Install Selected Packages.sh
+++ dev-mobile/ubuntu-14.04/sequence/2. Install Selected Packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PACKAGES=$(read_lst "./conf/packages.lst")
+packages=$(read_lst "./conf/packages.lst")
 
-printf %s "$PACKAGES" | while IFS= read -r package; do
+printf %s "${packages}" | while IFS= read -r package; do
 	if test -x "./pre/${package}.sh"; then
 		echo 
 		echo "There is a script to run before installing ${package}."
 		echo "Executing ./pre/${package}.sh..."
-		./pre/${package}.sh
+		"./pre/${package}.sh"
 		echo
 	fi
 
@@ -18,7 +18,7 @@ printf %s "$PACKAGES" | while IFS= read -r package; do
 		echo
 		echo "There is a script to run after installing ${package}."
 		echo "Executing ./post/${package}.sh..."
-		./post/${package}.sh
+		"./post/${package}.sh"
 		echo
 	fi
 done
