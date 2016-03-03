9ec99cea36d39529ef2fa83ec74d488a1f51df4f
xcjs <zlowery@xcjs.com>
2016-03-02 19:36:57 -0500

    Added echo statements to sequence 6 as previously there were none.

--- dev-mobile/ubuntu-14.04/sequence/6. Create Custom Home Directories.sh
+++ dev-mobile/ubuntu-14.04/sequence/6. Create Custom Home Directories.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Making custom home directories..."
 mkdir -p ~/bin ~/Projects ~/src ~/.icons ~/.themes ~/Pictures/UI
 
+echo "Linking local theme resources for gksu application support..."
 # Link local icons and themes folder so that gksu applications still look correct.
 cd ~/
 sudo ln -s .icons /root/.icons
