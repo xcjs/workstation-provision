8de9220c4581423b0bf0ee3dc256dbbb01579588
xcjs <zlowery@xcjs.com>
2016-03-02 19:47:15 -0500

    Changed password casing in the Visual Studio Code installer.

--- dev-mobile/ubuntu-14.04/installers/visualstudiocode.sh
+++ dev-mobile/ubuntu-14.04/installers/visualstudiocode.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-VERSION=0.10.6-release
-FILE=VSCode-linux64.zip
-INSTALLPATH=vscode-editor
-SYMLINK=vscode
+version=0.10.6-release
+file=VSCode-linux64.zip
+installPath=vscode-editor
+symLink=vscode
 
-ORIGINALPATH=$(pwd)
+originalPath=$(pwd)
 
 echo "= Install Visual Studio Code ============================================"
 
 mkdir -p ~/bin
 cd ~/bin
 
-echo "Downloading Visual Studio Code ${VERSION}..."
-wget -q https://az764295.vo.msecnd.net/public/${VERSION}/${FILE}
+echo "Downloading Visual Studio Code ${version}..."
+wget -q https://az764295.vo.msecnd.net/public/${version}/${file}
 
 echo "Extracting Visual Studio Code..."
-unzip -qq $FILE -d ${INSTALLPATH}
+unzip -qq $file -d ${installPath}
 
 echo "Playing musical chairs with the Visual Studio Code directory..."
-VSCODEDIR=$(ls vscode-editor/ | head -1)
-mv ${INSTALLPATH}/*/* ./${INSTALLPATH}/
-rmdir ${INSTALLPATH}/${VSCODEDIR}
+vscodeDir=$(ls vscode-editor/ | head -1)
+mv ${installPath}/*/* ./${installPath}/
+rmdir ${installPath}/${vscodeDir}
 
 echo "Cleaning up the archive..."
-rm ${FILE}
+rm ${file}
 
 echo "Symlinking Visual Studio Code..."
-ln -s ${INSTALLPATH}/vscode ${SYMLINK}
+ln -s ${installPath}/vscode ${symLink}
 
 echo
 
-cd ${ORIGINALPATH}
+cd ${originalPath}
