3b30be4b9815509eb49e0e825f0cb60512f8b014
xcjs <zlowery@xcjs.com>
2016-03-02 19:38:02 -0500

    Properly escaped the ppas/ppa variables.

--- dev-mobile/ubuntu-14.04/sequence/3. Add Personal Package Archives.sh
+++ dev-mobile/ubuntu-14.04/sequence/3. Add Personal Package Archives.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PPAS=$(read_lst "./conf/ppas.lst")
+ppas=$(read_lst "./conf/ppas.lst")
 
-printf %s "$PPAS" | while IFS= read -r ppa; do
-	echo "Adding package archive $ppa..."
-	sudo -E add-apt-repository -y $ppa  > /dev/null 2>&1
+printf %s "${ppas}" | while IFS= read -r ppa; do
+	echo "Adding package archive ${ppa}..."
+	sudo -E add-apt-repository -y "${ppa}"  > /dev/null 2>&1
 done
