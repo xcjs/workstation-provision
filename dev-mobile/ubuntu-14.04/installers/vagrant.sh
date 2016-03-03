2c495daecb779bda2f16245c43ae6aad35126dcb
xcjs <zlowery@xcjs.com>
2016-03-02 19:57:23 -0500

    Broke the Ruby gem prereqs up onto multiple lines.

--- dev-mobile/ubuntu-14.04/installers/vagrant.sh
+++ dev-mobile/ubuntu-14.04/installers/vagrant.sh
@@ -17,7 +17,25 @@ echo "Cleaning up..."
 rm "${file}"
 
 echo "Installing Ruby gem prerequsisites..."
-sudo apt-get -qq install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool > /dev/null 2>&1
+sudo apt-get -qq install \
+	build-essential \
+	bison \
+	openssl \
+	libreadline6 \
+	libreadline6-dev \
+	curl \
+	git-core \
+	zlib1g \
+	zlib1g-dev \
+	libssl-dev \
+	libyaml-dev \
+	libxml2-dev \
+	autoconf \
+	libc6-dev \
+	ncurses-dev \
+	automake \
+	libtool \
+> /dev/null 2>&1
 
 echo "Refreshing the command cache..."
 hash -r
