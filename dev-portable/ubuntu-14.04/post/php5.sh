846f41a9b0bc003883adcf569a34a87a638ac81e
xcjs <zlowery@xcjs.com>
2016-03-06 14:51:21 -0500

    Closes #16. Added tools for Laravel development.

--- dev-portable/ubuntu-14.04/post/php5.sh
+++ dev-portable/ubuntu-14.04/post/php5.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Install Composer and add it to the path
+echo "Installing Composer..."
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin
+
+echo "Adding Composer alias to the .bash_aliases file..."
 touch ~/.bash_aliases
 echo "alias composer='php /usr/local/bin/composer.phar'" >> ~/.bash_aliases
 source ~/.bash_aliases
+
+echo "Installing Laravel Installer..."
+composer global require "laravel/installer=~1.1"
