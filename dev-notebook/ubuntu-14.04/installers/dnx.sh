dd6acb98eca9cf6537662b41beaaff0bd7480c09
xcjs <zlowery@xcjs.com>
2016-03-06 17:34:55 -0500

    Updating the .NET Core installer to use the release deb channel.

--- dev-notebook/ubuntu-14.04/installers/dnx.sh
+++ dev-notebook/ubuntu-14.04/installers/dnx.sh
@@ -2,45 +2,18 @@
 
 # This installs ASP.NET 5/.NET Core and is almost a copy-paste of the
 # official instructions found online at
-# http://docs.asp.net/en/latest/getting-started/installing-on-linux.html
+# http://dotnet.github.io/getting-started/
 
 echo "= Install .NET Core ====================================================="
 
-originalPath=$(pwd)
-cd ~/
+echo "Adding the .NET software channel..."
+sudo sh -c 'echo "deb [arch=amd64] http://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 
-echo "Installing the .NET Core prerequsisites..."
-sudo apt-get -qq install \
-	unzip \
-	curl \
-	libunwind8 \
-	gettext \
-	libssl-dev \
-	libcurl4-openssl-dev \
-	zlib1g \
-	libicu-dev \
-	uuid-dev \
-	make \
-	automake \
-	libtool \
-	curl
-> /dev/null 2>&1
+echo "Adding the .NET apt key..."
+sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893 > /dev/null
 
-echo "Downloading the dnvminstall.sh script and sourcing it..."
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/release/dnvminstall.sh | DNX_BRANCH=release sh && source ~/.dnx/dnvm/dnvm.sh
+echo "Updating package lists..."
+sudo apt-get update > /dev/null
 
-echo "Installing the .NET Core CLR..."
-dnvm upgrade -r coreclr
-
-echo "Building Libuv"
-curl -sSL https://github.com/libuv/libuv/archive/v1.8.0.tar.gz | sudo tar zxfv - -C /usr/local/src
-
-cd /usr/local/src/libuv-1.8.0
-sudo sh autogen.sh
-sudo ./configure
-sudo make
-sudo make install
-sudo rm -rf /usr/local/src/libuv-1.8.0 && cd ~/
-sudo ldconfig
-
-cd "${originalPath}"
+echo "Installing .NET Core..."
+sudo apt-get -qq install dotnet=1.0.0.001598-1 > /dev/null 2>&1
