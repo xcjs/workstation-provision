1fc618c9971f232bd37644f935b3d9eb0e403fd1
xcjs <zlowery@xcjs.com>
2016-03-06 15:34:39 -0500

    Added a PyCharm installer.

---
+++ dev-portable/ubuntu-14.04/installers/pycharm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+file=pycharm-professional-5.0.4.tar.gz
+installPath=pycharm-ide
+
+originalPath=$(pwd)
+
+echo "= Install PyCharm ======================================================"
+
+mkdir -p ~/bin
+cd ~/bin
+
+echo "Downloading PyCharm..."
+wget -q "https://download.jetbrains.com/python/${file}"
+
+echo "Extracting PyCharm..."
+mkdir -p "${installPath}"
+tar -zxvf "${file}" --strip-components=1 -C "${installPath}" > /dev/null
+
+echo "Cleaning up the PyCharm archive..."
+rm "${file}"
+
+echo "Symlinking PyCharm..."
+ln -s "${installPath}/bin/idea.sh" pycharm
+
+cd "${originalPath}"
+
+echo
