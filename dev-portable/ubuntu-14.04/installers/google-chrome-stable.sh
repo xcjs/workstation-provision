e96371790c0e59010e8eb59857d3113ff6aacf0e
xcjs <zlowery@xcjs.com>
2016-03-06 14:48:13 -0500

    Corrected the Google Chrome deb channel to only look for the 64 bit build only.

--- dev-portable/ubuntu-14.04/installers/google-chrome-stable.sh
+++ dev-portable/ubuntu-14.04/installers/google-chrome-stable.sh
@@ -5,7 +5,7 @@ echo "= Install Google Chrome ================================================="
 # Grab the offical Google Chrome browser
 echo "Adding Google's official Chrome channel..."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 echo "Installing Google Chrome..."
 sudo apt-get -qq update && sudo apt-get -qq install google-chrome-stable
