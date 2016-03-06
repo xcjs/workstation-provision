8144321bd51b6760f34c9468b1705fb3e9eebc87
xcjs <zlowery@xcjs.com>
2016-03-06 15:56:12 -0500

    Solves #40. Added WebStorm installer.

---
+++ dev-portable/ubuntu-14.04/installers/webstorm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+file=WebStorm-11.0.3.tar.gz
+installPath=webstorm-ide
+
+originalPath=$(pwd)
+
+echo "= Install WebStorm ======================================================"
+
+mkdir -p ~/bin
+cd ~/bin
+
+echo "Downloading WebStorm..."
+wget -q "https://download.jetbrains.com/webstorm/${file}"
+https://download.jetbrains.com/webstorm/WebStorm-11.0.3.tar.gz
+
+
+echo "Extracting WebStorm..."
+mkdir -p "${installPath}"
+tar -zxvf "${file}" --strip-components=1 -C "${installPath}" > /dev/null
+
+echo "Cleaning up the WebStorm archive..."
+rm "${file}"
+
+echo "Symlinking WebStorm..."
+ln -s "${installPath}/bin/webstorm.sh" webstorm
+
+cd "${originalPath}"
+
+echo
