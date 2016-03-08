edb632d3944c23eefe595b80cbd479a355aa582d
xcjs <zlowery@xcjs.com>
2016-03-07 20:34:22 -0500

    This solves #44. Added Redis Desktop Manager.

---
+++ dev-notebook/ubuntu-14.04/installers/redis-desktop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+0.8.3/redis-desktop-manager_0.8.3-120_amd64.deb
+
+version=0.8.3
+file=redis-desktop-manager_0.8.3-120_amd64.deb
+
+originalPath=$(pwd)
+
+echo "= Install Redis Desktop Manager ========================================="
+
+echo "Downloading Redis Desktop Manager..."
+wget -q "https://github.com/uglide/RedisDesktopManager/releases/download/${version}/${file}"
+
+sudo dpkg -i ${file}
+rm ${file}
+
+cd "${originalPath}"
+
+echo
