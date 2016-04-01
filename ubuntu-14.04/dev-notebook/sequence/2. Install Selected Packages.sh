318b70704a801db125b883c08113a10c53e0c5ad
xcjs <zlowery@xcjs.com>
2016-04-01 18:59:38 -0400

    Added extra space to the beginning of pre/post package install messges.

--- ubuntu-14.04/dev-notebook/sequence/2. Install Selected Packages.sh
+++ ubuntu-14.04/dev-notebook/sequence/2. Install Selected Packages.sh
@@ -5,6 +5,7 @@ packages=$(read_lst "./lst/packages.lst")
 printf %s "${packages}" | while IFS= read -r package; do
 	if test -x "./pre/${package}.sh"; then
 		echo
+		echo
 		echo "There is a script to run before installing ${package}."
 		echo "Executing ./pre/${package}.sh..."
 		"./pre/${package}.sh"
@@ -15,6 +16,7 @@ printf %s "${packages}" | while IFS= read -r package; do
 	sudo -E apt-get -qq install $package > /dev/null 2>&1
 
 	if test -x "./post/${package}.sh"; then
+		echo
 		echo
 		echo "There is a script to run after installing ${package}."
 		echo "Executing ./post/${package}.sh..."
