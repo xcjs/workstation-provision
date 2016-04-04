76d696ab3bb320674b02d24f01ef041c01c4256d
xcjs <zlowery@xcjs.com>
2016-04-04 18:40:54 -0400

    Swapped sequences 6 and 7.

--- ubuntu-14.04/dev-notebook/sequence/7. Create Custom Home Directories.sh
+++ ubuntu-14.04/dev-notebook/sequence/7. Create Custom Home Directories.sh
@@ -3,13 +3,13 @@
 originalPath=$(pwd)
 
 echo "Making custom home directories..."
-mkdir -p ~/Auth ~/bin ~/Projects ~/src ~/.icons ~/.themes ~/Pictures/UI
+mkdir -p ~/Auth ~/Projects ~/src ~/.icons ~/.themes ~/Pictures/UI
 
 echo "Linking local theme resources for gksu application support..."
 # Link local icons and themes folder so that gksu applications still look correct.
 cd ~/
 sudo ln -s .icons /root/.icons
-sudo ln -s .themes /root/.themes 
+sudo ln -s .themes /root/.themes
 
 echo "Linking the Flattr icon themes..."
 ln -s .icons/flattr-icons/Flattr .icons/Flattr
