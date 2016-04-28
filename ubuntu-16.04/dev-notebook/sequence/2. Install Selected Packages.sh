ff0f96fa49f8d8a5d93576ef9e27b0d56b667f36
xcjs <zlowery@xcjs.com>
2016-04-27 21:18:05 -0400

    Removed extra space before pre/post install tasks for packages.lst.

--- ubuntu-16.04/dev-notebook/sequence/2. Install Selected Packages.sh
+++ ubuntu-16.04/dev-notebook/sequence/2. Install Selected Packages.sh
@@ -5,7 +5,6 @@ packages=$(read_lst "./lst/packages.lst")
 printf %s "${packages}" | while IFS= read -r package; do
 	if test -x "./pre/${package}.sh"; then
 		echo
-		echo
 		echo "There is a script to run before installing ${package}."
 		echo "Executing ./pre/${package}.sh..."
 		"./pre/${package}.sh"
@@ -16,7 +15,6 @@ printf %s "${packages}" | while IFS= read -r package; do
 	sudo -E apt-get -qq install $package > /dev/null 2>&1
 
 	if test -x "./post/${package}.sh"; then
-		echo
 		echo
 		echo "There is a script to run after installing ${package}."
 		echo "Executing ./post/${package}.sh..."
