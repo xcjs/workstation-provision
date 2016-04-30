3ae6b04b2727ff98dac92687797f3428422746b5
xcjs <zlowery@xcjs.com>
2016-04-30 16:34:01 -0400

    Removed the Virtualbox update from the 14.04 script.

--- ubuntu-14.04/dev-notebook/installers/virtualbox-5.0.sh
+++ ubuntu-14.04/dev-notebook/installers/virtualbox-5.0.sh
@@ -4,8 +4,8 @@ echo "= Install Virtualbox 5.0 ================================================"
 
 # Grab the latest version of Virtualbox from the Oracle repository.
 echo "Adding Oracle channel for Virtualbox 5.0..."
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list.d/virtualbox.list'
 
 echo "Installing Virtualbox 5.0..."
 sudo apt-get -qq update && sudo apt-get -qq install virtualbox-5.0
