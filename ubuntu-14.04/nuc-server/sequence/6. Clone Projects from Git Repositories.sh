ab5d20898c8b19f57dd8f33c6821e3d344d6afae
xcjs <zlowery@xcjs.com>
2016-07-17 16:21:08 -0400

    Sudo-ed the git clone command so that projects can be cloned under other users.

--- ubuntu-14.04/nuc-server/sequence/6. Clone Projects from Git Repositories.sh
+++ ubuntu-14.04/nuc-server/sequence/6. Clone Projects from Git Repositories.sh
@@ -8,7 +8,7 @@ cd ~/
 printf %s "${gitRepos}" | while IFS= read -r repo; do
 	repo=($repo)
 	echo "Cloning $repo..."
-	eval git clone "${repo[@]}"
+	eval sudo git clone "${repo[@]}"
 done
 
 cd "${originalPath}"
